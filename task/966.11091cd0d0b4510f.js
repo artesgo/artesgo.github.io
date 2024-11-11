453278057016421bb05f3443ad9e2b8e9bc5e2cf
artesgo <artesgo@gmail.com>
2024-11-11 15:04:55 -0500

    gh pages the new way?

---
+++ task/966.11091cd0d0b4510f.js
@@ -0,0 +1,1 @@
+(self.webpackChunktask=self.webpackChunktask||[]).push([[966],{8966:(c,d,o)=>{var n=o(3424),e=o(9381),s=o(1703);const a=[{path:"",loadChildren:()=>o.e(76).then(o.bind(o,7673)).then(t=>t.remoteRoutes)}],p={providers:[(0,e.provideZoneChangeDetection)({eventCoalescing:!0}),(0,s.provideRouter)(a)]};var r=o(6528);(0,n.bootstrapApplication)(r.a,p).catch(t=>console.error(t))}}]);
